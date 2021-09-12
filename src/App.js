75bd64417e128b41ed41e173a1225e705b3cbf4e
younghun1124 <83543030+younghun1124@users.noreply.github.com>
2021-09-12 17:27:45 +0900

    lecture 4.3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import React from "react";
-import Potato from "./Potato";
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Potato />
-    </div>
-  );
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
